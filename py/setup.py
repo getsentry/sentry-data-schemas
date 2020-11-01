4732ecb885f984c82dc73399de4bfae5ac358817
Filippo Pacifici <filippo.pacifici@gmail.com>
2020-10-31 18:05:31 -0700

    Remove file check

--- py/setup.py
+++ py/setup.py
@@ -8,17 +8,17 @@ def get_requirements():
 
 # Copies the schema in the module so that setuptools is able to
 # find the file and add it to the package.
-if os.path.isfile("../relay/event.schema.json"):
-    shutil.copyfile(
-        "../relay/event.schema.json",
-        "./sentry_data_schemas/event.schema.json"
-    )
+#if os.path.isfile("../relay/event.schema.json"):
+shutil.copyfile(
+    "../relay/event.schema.json",
+    "./sentry_data_schemas/event.schema.json"
+)
 
-if os.path.isfile("../LICENSE"):
-    shutil.copyfile(
-        "../LICENSE",
-        "./LICENSE"
-    )
+#if os.path.isfile("../LICENSE"):
+shutil.copyfile(
+    "../LICENSE",
+    "./LICENSE"
+)
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
